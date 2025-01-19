44ce9af19ece5355f65ea09e615c8da9319517d6
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-01-19 20:05:56 +0100

    Add files via upload

---
+++ ADV_L1_Non_Attend.py
@@ -0,0 +1,674 @@
+
+"""
+
+In The Name of GOD
+
+Created on Sun Jan 19 18:44:00 2025
+
+@author: Ali Pilehvar Meibody
+
+
+intro:
+    
+    
+Machine ( binary 0,1) --------- intermediate language(python) ----- human(EN)
+
+
+python-->language
+vocabs,
+grammar
+
+
+
+IDE--> SPYDER TOOSH CODE
+
+CODE TA ZAMANI K RUN NZNI SEND NASHODE
+
+
+do sorate
+kolesh run mishe
+
+run mizni -->python mese yek ensan
+az bala b paeen | chap b rast mikhoone
+
+# quotation mipare ignore mikone
+
+vase inke comment, note , instruction
+
+"""
+
+'''
+harchi bnvisi az bala b paeen mikhone va behet gir mide ag
+zaboonesh nabashe
+
+reserved vocabs --> kalamati k reserved shdo eestefade koni
+
+
+
+1-----> python built in function (narenji) -->tabe haye dakheli hast k yekseri amalkard dare (namayesh , chizi migire,...)
+2----> dastoorat ( logic , mantegh , if , for , ....) -->banafsh
+3--->harchi joz ina --> sefid --> zarf (variable)--> toosh yechizi berizi zakhrie koni
+
+esme zarfe ro
+
+
+'''
+#--------1 python built in fucntion
+print()
+input()
+open()
+len()
+type()
+#100
+
+#-----2----
+if
+for
+while
+
+#---3---
+sdsklj
+daskjdhsd
+sdakjsha
+dsadsa
+dsadasjh
+
+
+age=40
+name='ali'
+
+
+#---------Tooye esme zarf ham behet gir mide
+#mige harchizi bekhaynmishe ghanon drm
+
+name='ali'
+age=40
+
+#adad aval bnvisi ya character
+#ghazlat
+45name=
+^&name=
+
+#tzahesh
+name2=50
+
+
+#case sensetive a ba A frgh dre
+a=20
+A=30
+
+#reserv shdoe nmitoni bzri
+print=
+printt=
+print2=
+
+
+#space ham ghabol nmikone 
+#character nmitoni bzari
+#underline _
+
+#esmam myname 
+
+#1--> benvesii 
+myhatchedparameter=
+#ghalat
+my hatched parameter=
+
+#y ravesh bchsbon fght avalesho caps
+MyHatchedParameter=
+MyAge=
+
+#ravesh _
+my_age=40
+My_Age=50
+
+
+#------------------------------------------
+#yad grftm k harchizi benevbisam fght sefid mishe
+#zarf hast --> esmesh ghanon dasre in zarf
+
+#zarf?? (variable) --> yek value (megdhar) dakheelsh zkaahire konam
+#valuye ha chian??
+
+#3.1. Numbers ( int, float, complex)
+#int ---> sahih -->< bneshmori , ashar nadare 4.5 nafar ensan
+
+myage=30
+myage=35
+
+#float -_.ashari
+myweight=45.6
+
+#adad --> i + j -->mohandesi
+b=4+3j
+
+myage=40
+#= beriz too zarf
+
+
+
+
+#hchaarcter = + - * /
+#ina baraye amaliatan
+#calculation 
+
+#a+b -->+
+'''
+()
+** tavan
+* zarb
+/ taghsim
++ jam
+- tafrigh
+
+
+% baghimande
+
+
+'''
+a=20
+b=30
+#aval zarf bzri bad baghie ro toosh brizi
+a+b=c
+
+c=a+b
+
+#**
+
+c=4**2*3+8
+c=((4**2)+8)*3
+
+
+
+#======
+'''
+aya abrabar hast?
+==
+
+!=
+
+>
+
+>=
+
+<
+
+<=
+
+
+
+
+'''
+
+#dastoor
+a=2
+a=b+c
+#b ro be alave c bokon 
+
+#miporsiii
+
+
+
+a=2
+
+#aya a barabar hast ba 2??
+#montazere javabi
+a==2
+# True
+
+
+#3.2.str --> hrf, kalame, jomle , ketab ,....
+
+name='a'
+name='ali'
+name='alipilehvar meibody'
+name='salam khob hastid khosh amadid be jalase a'
+
+
+a=2
+b='2'
+
+
+
+
+#boolean --> 0 1 true false
+name=True
+name=False
+
+
+
+
+
+#----------------------
+#built in function-------
+#karbord
+
+#namayehs-->print
+#chizi begrii ba input
+
+#jhamashoon parantez daran () --> function
+
+
+print('salam')
+
+#salam
+
+
+a='salam'
+
+print(a)
+#salam
+
+
+
+a='salam'
+b='khobi'
+
+#commaaaa
+print(a,b)
+#salam khobi
+
+#aya man mitonam taghir bdm??
+#avali ro bvnvis badio y setare bezan beynesh???
+
+print(a,b,sep=' ')
+#salam khobi
+
+print(a,b,sep='*')
+
+#salam*khobi
+
+
+print(a,b,sep='-----')
+#salam-----khobi
+
+
+a='salam'
+b='khoobi'
+
+print(a)
+print(b)
+
+
+
+
+print(a,end='')
+print(b)
+#salamkhoobi
+
+
+print(a,end='********8')
+print(b)
+#salam********8khoobi
+
+
+
+
+#----input
+#migire az karbar adad , ...... tooye console
+
+input('salam senetoon cheghadr hast?')
+#yechi minevise va yechi migire az karbar
+#haal ino migire bayad too ychizi brizi zakhriesh koni??
+
+#zarf-> variable
+
+answer=input('salam senetoon cheghadre :')
+
+#noktweye mohem ine hamechioo string zakhire mikonee
+
+
+
+answer=input('salam senetoon cheghadre :')
+final=answer/10
+print(final)
+
+#TypeError: unsupported operand type(s) for /: 'str' and 'int'
+
+
+#rahkar
+
+answer=input('salam senetoon cheghadre :')
+
+new_answer=int(answer)
+
+final=new_answer/10
+
+print(final)
+
+
+#----zarf nasaz
+
+
+answer=int(input('salam senetoon cheghadre :'))
+
+#minevise salam senetoon cheghadre
+#az taraf migire 
+#int mikone
+#mirize too answer
+
+
+answer=input('salam senetoon cheghadre :')
+ 
+print(type(answer)) #<class 'str'>
+
+new_answer=int(answer)
+
+print(type(new_answer))
+#<class 'int'>
+
+final=new_answer/10
+
+print(final)
+
+
+
+name='alipilehvarmeibody'
+
+print(len(name)) #18
+
+
+#space ham hesab mikone
+
+
+name='ali pilehvar meibody'
+
+print(len(name)) #20 2 ta space ham hesab mikoneeee
+
+
+
+
+'''
+machine(0,1) -----(python) ----- human(en)
+
+python --> vocabs
+
+vocabs -->3 no
+1-->python built in function (print,input,len,type,..)
+2-->keywords ( if , else, for , ....)
+3-->variables (zarf) -->3.1.1.int,2.float,3.complex, 3.2.str
+
+****python built in funciton 
+esm ro minevsii y parantez -->parantez --> tabe
+baraye hame hast , int, boolean , str ,....
+
+print()
+
+tabe ye dg darim k fght baraye str , list, tuple ,...
+str function (nemigan tabeye dakheli)
+
+
+
+
+'''
+#======================================
+#======================================
+#======================================
+#======================================
+#======================================
+#======================================
+#======================================
+#======================================
+
+#------str------
+
+name='alipilehvar'
+
+#---> aya man m,itonam dastresi dahste basham b horof?
+#bale --> str 
+#index
+
+#esme on zarf [] beeraket bzn shoamre index
+
+#index --> chandomio mikhay (az 0 shoro mishe)
+
+#2vomin harf ro mikham
+# 1 2
+
+# 0 1 2 3 4
+
+print(len(name))
+#11
+
+#11 ta character
+
+#a l i p i l e h v a r
+#0 1 2 3 4 5 6 7 8 9 10
+
+name[]
+
+#inja migam kodom index
+
+#dovomio mikham?
+#0 na , 1
+name[1]
+# 'l'
+
+name[7]
+#'h'
+
+#choon ya mikhay behesh dastresi peyda koni zakhriash koni
+
+haftom=name[7]
+
+print(haftom)
+#h
+
+name[3]
+
+#az 3 ta felan
+
+#a l i p i l e h v a r
+#0 1 2 3 4 5 6 7 8 9 10
+
+#p i l --> 3 ta 5
+#do noghte hamishe taa ro mide
+'''
+access
+
+variable_name[start:end+1]
+
+'''
+name[3:5]
+# 3 va 4 --> oon akahrie exclude dar nazar nmigire
+
+#$ag mikhay bgi 3 , 4 , 5 
+#3:6 --> 3,4,5  6 na
+
+name[3:6]
+
+
+#hosele nadari bgi az 0 ta 6
+name[0:7] #'alipile'
+name[:7] #'alipile'
+
+
+name[4:11] #'ilehvar'
+name[4:] #'ilehvar'
+
+'''
+access
+
+variable_name[start:end+1:step]
+
+default step = 1
+
+2ta 8 --> yedone yedone 2 , 3 , 4 ,5 ,6 ,7 
+
+'''
+name[2:8] #'ipileh'
+name[2:8:1] #'ipileh'
+#a z2 ta 8-1 1 done 1 done
+
+#2 4 6 
+name[2:8:2]
+#'iie'
+
+
+
+
+#ino zakhire konam
+#man mikham az esmam az aval t tahesho done done 2 ta 2 ta joda konm 
+#brize too yek zarf bename new_name
+
+
+name='alipilehvarmeibody'
+print(len(name))
+new_name=name[2:18]
+print(new_name) #ipilehvarmeibody
+
+
+#professional
+name='alipilehvarmeibody'
+new_name=name[2:]
+print(new_name) #ipilehvarmeibody
+
+
+
+#-----------------
+name='alipilehvarmeibody'
+print(len(name))
+new_name=name[2:18:2]
+print(new_name) #iievrebd
+
+#2 ta tahe 2 ta 2 ta
+new_name=name[2::2]
+print(new_name)
+
+
+#===========================
+#dastresii----> index[]
+#zakhrie
+
+#mikham chnage konam taghir bdm
+
+
+
+#taghriehs bdii
+
+#a l i p indexe 3
+#p-->b
+name[3]='b'
+
+#TypeError: 'str' object does not support item assignment
+
+#bozorg kjonam
+#kochikesh konam
+#p kochi
+
+#p kojas
+#........
+
+#str functions---> tabe haye str
+#az inha mitonam estefade konam......
+
+
+
+#tabe haee hast k mitonii taghirat bdiiii
+#in tabe ha behesh mign str function
+#va faghat baraye str hastan
+
+
+#python built in funciton -->print() ->baraye hame str, int , list,..
+
+name='ali'
+
+#upper mese print
+#naaaa
+upper(name)
+
+print(name) #tabe haye daskheli ro injori minevisi
+
+#tabe haye str
+
+#rooye oon zarfi k mikhay taghiresh bdii dot mizni
+
+#dforostttt
+name.upper()
+#'ALIPILEHVARMEIBODY'
+
+#ino zakhrie kon
+
+new_name=name.upper()
+print(new_name)
+#ALIPILEHVARMEIBODY
+
+
+new_name.lower() #'alipilehvarmeibody'
+
+name.title()
+
+
+sentence='in the name of god'
+
+#print()  print(sentence) --> print() input() len() type()
+title(sentecne) #in ghalatyeeeee 
+
+sentence.title()
+#'In The Name Of God'
+
+
+a=20
+a.upper()
+#AttributeError: 'int' object has no attribute 'upper'
+
+#int upper ndre
+
+#str upper dre
+
+
+'''
+2 ta nokte
+
+str function
+
+frghesh chie
+
+1--> fghtr baraye str ha hastan 
+
+2--> upper(sentence)  dot mizani
+
+sentence.upper()
+
+
+
+
+
+#liste bozorg
+upper()
+lower()
+strip()
+split()
+find()
+replace()
+title()
+
+........
+
+
+
+zarf --> toosh vbalue , adad, str 
+chanta bekhaym brizim chi?? --> list, .....
+
+list , tabe haye list --> 
+oon application baham kar konim list yham dakhilesh mikonim
+
+if , if else,......
+
+
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
