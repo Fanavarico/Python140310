fb1d16d6be5450c70e09a34659c6d953b997e345
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-03-09 20:16:16 +0330

    Add files via upload

---
+++ ADV_L4_Non_Attend.py
@@ -0,0 +1,1461 @@
+
+"""
+Created on Sun Mar  2 17:35:30 2025
+
+@author: apm
+
+
+ADV_L4_Non_Attend
+
+
+
+
+"""
+
+
+
+
+'''
+review-------
+python
+
+reserved----------
+
+python built in function print() input() ,..
+
+kjeywrods --> if else elif , for , while ,...
+
+
+unreserved-----------
+zarf-->variable , moteghayer
+(numbers(int,float)) , bool(True,False)
+str ( --> [0] , [start:end+1:step] , str fucntions
+     .upper()  -->emal nmsihod, khoroji mdiad)
+
+iterables--> chan element dare
+list --> a=[el1,el2,el3]
+el[index] , list function --> emal .append()
+.clear()  .insert()
+
+order, changable,allow duplciated
+list, set { } , tuple (,) , dictioanry [key]
+tabe haye khodeshon ro daran
+
+
+
+
+
+
+
+
+
+
+----reserved--keywrods-----
+IF
+
+
+
+
+
+
+
+
+
+'''
+
+print('salam')
+
+
+#tooye codet taghir ->keywords
+
+
+#rahzan biad vasay ye shartio check kone
+
+'''
+just if
+
+
+if shart:
+    dastooor
+
+
+
+shart --> True False
+== > < >= <= !=
+tabe haee khdoet msiaz
+tabe haye tabe str -->True False
+isupper()
+
+na
+
+chanta shart--> and or
+
+ya -->or hadeaghal yeki az chanta shartet
+hameye shartat -->va --> and
+
+
+if shart1 or shart2 or shart3:
+    dastooor
+    
+    
+if shart1 and shart2 and shart3:
+    dastooor
+    
+
+
+if shart:
+    dastoor1
+    dastoor2
+    dastoor3
+    .....
+    
+    
+#inja dg shart nis
+
+baraye hame besorate adi ejr amishe
+    
+
+
+shartt ->True bshe dastor ejra mishe
+ag true nashe-->hichi ejra 
+
+
+
+
+gahi mikhay toye coet dorahi baz koni
+begi age shart true shdoe kare1 ag nqashdo kare 2
+
+
+if else
+
+
+if shart:
+    dastoor
+    dastoor
+    (ag shartet True bshe)
+    
+else:
+    dastoor
+    dastoopr
+    
+    
+    
+#---do rahi baz mikoni
+#Mitoni dorahit to rahe dovome ye dorahi dg baz
+derakh
+
+if elif
+
+if elif else
+
+'''
+
+
+
+sen=float(input('salam senet:'))
+
+if sen>18:
+    print('shoma gjhanooni')
+
+#chizi printnmsihe
+
+
+
+
+
+sen=float(input('salam senet:'))
+
+if sen>18:
+    print('shoma gjhanooni')
+else:
+    print('shoma ghanoni nisti')
+
+
+
+#----------
+sen=float(input('salam senet:'))
+
+if sen>18:
+    print('shoma gjhanooni')
+
+elif sen>15:
+    print('sdsd')
+else:
+    print('dsdsasadsda')
+    
+    
+    
+#---------------------------------------
+
+#------PLUTUS MANAGEMENT PRODUCT MANAGEMENT------
+
+code=input('lotfan code mahsol bede:')
+name=input('lotfan name mahsol bede:')
+price=input('lotfan gheymate mahsool bede')
+
+
+text=f'''
+
+----plutus management------
+product code: {code}
+    
+product name: {name}
+    
+    
+    total price: {price}
+        
+
+Co. 2024
+'''
+
+print(text)
+
+
+#-------
+code=input('lotfan code mahsol bede:')
+name=input('lotfan name mahsol bede:')
+price=input('lotfan gheymate mahsool bede')
+
+if price.isdigit():
+    text=f'''
+
+    ----plutus management------
+    product code: {code}
+        
+    product name: {name}
+        
+        
+        total price: {0.7*float(price)}
+            
+
+    Co. 2024
+    '''
+
+    print(text)
+    
+    
+else:
+    print('lotfan adad vared kon')
+
+
+#-----------------------------------
+
+
+'''
+
+------- HALGHE ---------
+
+
+Reserved---->keywords---->codo taghir mdian
+
+if --> banafsh
+
+
+'''
+
+print('salam')
+
+
+if sen>20:
+    print('salam')
+
+
+
+
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+
+
+#---------------
+#python
+#c++
+#dart
+#....
+
+
+
+
+#AVALIN KILIDVAZHE--->TEKRAR-->HALGHE
+
+#
+
+
+
+
+print('salam')
+
+
+
+#har khatiiii
+#khate code
+
+#code-->print('salam')
+
+'''
+1---> Shomarande
+2--->mahdoode
+3--->dastooor
+
+
+dastoooor
+
+python khodesh
+niaz dare b yechizi bname shomarande
+
+shomarande --> vase pythone
+komakie python
+age nabashe python nmitone halgeh ebsaze
+tekrar 
+
+
+
+shomarande
+y zarf --> k tedade inke chandomin bar codo ejra krde
+toosh zakhrie mikone
+
+
+
+zarf--.esm
+
+adad
+shomarande
+indexx
+indd
+
+sho
+
+k
+j
+i
+
+
+
+(i)
+
+python: y zarf bde man dastam bgirm
+hey tekrar chizi najma midm adade tekraro brizm dakheelsh
+yadam bemone kojaye tekrarama
+
+
+
+
+
+for i in range(0,10):
+    dastooor
+    
+
+
+
+shoamrandat 
+python dastoor
+chanbar???
+
+i python i 
+
+
+
+HALGHE
+
+shomarande ---> komak koni b python
+mahdooode----> man taeen mikonm
+dastooor----> on chizi k mikham hey tekrar bshe 
+
+
+
+for i in range(0,10)
+
+
+
+range(start,end+1)
+
+
+range(0,10)
+
+
+
+
+
+
+i
+0 1 2 3 4 5 6 7 8 9 -->mahdoode range(0,9)
+dastooor
+
+'''
+
+
+print('salam')
+
+if sen>18:
+    print('salam')
+
+
+
+#shomarande --->i
+#mahdode----> range(0,10)
+
+#shoro --> 0
+#akahrie shamel nmsieh (include) exclude
+
+name='alipilehvar'
+
+name[0]
+
+name[0:3] #'ali'
+#0 1 2
+
+#harja dot aadad
+
+#(include,exclude)
+
+
+
+#mahdoode
+#0 1 2 3 4 5 6 7 8 9
+
+
+print('salam')
+
+
+
+
+for i in range(0,10):
+    print('salam')
+
+#adiejra nkon, chikar kon?"
+
+
+'''
+1* -->python yekar mikone
+2*---> chizi k to mibinie
+
+0 1 2 3 4 5 6 7 8 9 
+
+1*                         2*
+i-->0   print(salam)    , salam
+i-->1   print(salam)    , salam
+i-->2  print(salam)   , salam
+....
+...
+i-->8  print(salam) , salam
+i-->9  print(salam)  , salam
+i-->10
+
+
+10 bar salamo mibini
+
+range(0,10)
+
+
+
+'''
+
+
+'''
+salam
+salam
+salam
+salam
+salam
+salam
+salam
+salam
+salam
+salam
+
+'''
+
+
+
+#shomrande
+#mahdoode
+#dastoooor
+
+
+
+#for
+#for i in .....
+
+#for i in range(0,10)
+
+#for i in 0 1 2 3 4 5 6 7 8 9
+
+
+#i -->0 -> dastooooro ejra mikoni
+#i--->1   dastooor
+
+
+
+
+
+
+for i in range(0,10):
+    print('salam')
+    
+#i -->taghir mdie
+#dastooro ejra
+
+#ide ye avali-->static looop
+
+#halgehey sabte
+
+
+#i tooye dsstore
+
+#dynamic
+
+for i in range(0,10):
+    print(i)
+    
+
+'''
+i-->0 --> print(i) print(0) --> 0
+i--.1
+..
+
+
+i-->10 XXXXX
+'''
+
+
+#-----
+for i in range(0,10):
+    print(i)
+print('salam')
+
+
+for i in range(0,10):
+    print(i)
+    print('salam')
+
+#NA TANAH YEK khat cxodo mitoni loop bzni
+#mitoni chan khat bzni
+
+
+'''
+
+shomarande
+mahdooode
+dastoooor
+
+
+for shoamrande in mahdoode:
+    dastoor(s)
+    
+    
+#inja nis
+
+
+
+mahdoode----> range(start,end+1)
+
+
+
+'''
+
+mylist=[0,1,2,3,4,5,6,7,8,9]
+#rnage(0,100)
+
+for i in mylist:
+    print('salam')
+
+
+#range()-->
+
+
+
+for i in mylist:
+    print('salam')
+
+'''
+i--0 , salam
+i.
+
+
+
+i-->9 salam
+
+
+
+
+'''
+
+
+myuser=['ali','mohsen','vahid','hamid']
+
+for i in myuser:
+    print('salam')
+
+
+
+
+'''
+i
+range
+
+i hey mizasht done doen y adadi
+va ******dastooro ejra mikrd
+
+
+1*                 2*
+i=ali dastoor  -->salam
+i=mohsen  dastooor ---> salam
+i=vahid  dastoor --->salam
+i=hamid dastooor-->saslamn
+
+
+
+4 bar salam chap msieh
+
+
+
+
+
+'''
+
+
+
+
+'''
+
+shoamrande
+mahdooode
+dastoooor
+
+
+
+for shomarande in mahdoode:
+    dastooor(s)
+    
+    
+shomarande--> i , k , item,....
+mahdoode--> range(start,end+1) , list, set , reshte (iterable)
+dastooor---> az shomarande-->static [tekrar] / dynamic [shoamrande]
+
+
+
+
+'''
+
+mylist=['ali','vahid','reza']
+
+for item in mylist:
+    print('salam')
+
+'''
+shomrande-->item
+mahdodoat-->ali / vahid / reza
+
+item-->ali  / print(salam) -->salam
+item-->vahid / print(salam) -->salam
+item-->reza /print(salam) -->salam
+
+
+
+
+
+'''
+
+mylist=['ali','vahid','reza']
+
+for item in mylist:
+    print(item)
+
+
+for i in mylist:
+    print(i)
+
+
+for esm in mylist:
+    print(esm)
+
+
+
+mylist=['ali','vahid','reza']
+
+for item in mylist:
+    print(i)
+    
+    
+
+'''
+
+item-->ali / print(i) -->
+
+NameError: name 'i' is not defined
+
+
+
+yek bar bartaye yamishe----
+python shomarane* dare dar yek mahdoode*
+done done shomarande ro mzire yek element az on mahdode
+bad dastooro ejra mikone
+
+dastor-->print(salam)
+shomrande
+
+
+
+'''
+
+
+name='ali'
+for i in name:
+    print('salam')
+
+'''
+i-->'a'   / salam
+i-->'l'  / salam
+i-->'i' / salam
+
+
+
+
+'''
+
+
+
+
+'''
+
+for shomarande in mahdoode:
+    dastooor
+    
+    
+range(0,10)
+
+iterable--> list element
+reshte --> reshte reshte ee az character ha
+a l i
+
+'''
+
+name='ali'
+for i in name:
+    print(i)
+
+'''
+a
+l
+i
+'''
+
+
+mylist=['ali']
+
+for i in mylist:
+    print('salam')
+    print(i)
+
+
+mynumbs=[10,20,30,40,50]
+
+
+for i in mynumbs:
+    print(i)
+
+
+
+print(10)
+print(20)
+
+'''
+10
+20
+'''
+
+
+#print(10,20)
+
+
+#2 ta print --> 10 , 20 knare ham chap konm?
+
+print('salam \n khobi')
+
+
+
+
+print(10,end='\n')
+print(20,end='\n')
+
+
+
+print(10,end=' ')
+print(20,end=' ')
+
+
+
+print(10,end='*')
+print(20,end='*')
+
+
+mynumbs=[10,20,30,40,50]
+
+
+for i in mynumbs:
+    print(i,end=' ')
+    
+    
+    
+    
+mylist=['ali','vahid','reza']
+for i in mylist:
+    print(i,end=' ')
+
+
+
+
+mylist=['ali','vahid','reza']
+print('salam arz shod be azizan',end=' ')
+
+for esm in mylist:
+    print(f'jenabe {esm} ',end=' ')
+
+#alam arz shod be azizan jenabe ali  jenabe vahid  jenabe reza  
+
+
+
+
+#-0--------------------
+
+
+#10
+#----->TEKRAR------
+for i in range(0,10):
+    print('salam')
+    
+    
+for i in range(0,10):
+    print(i)
+    
+    
+#iterable
+name='alipilehvar'
+
+mylist=[10,20,30,40,50,60]
+
+mylist=['vahid','ali']
+
+mylist=[True,10.5 ,1j , 10 , 'aLI']
+
+mytuple=(10,20,30,40,50)
+
+
+
+for i in mylist:
+    print(i)
+
+
+
+#---------> iteration --> varresii , 
+mylist=['vahid','ali','REZA','hamid']
+
+for i in mylist:
+    print(i)
+
+
+#iteration-->bere dakhele yelk listi chizi
+#done done check kone
+
+#check--> if
+
+
+
+mynumbs=[10,20,30,40,50,60,70,80,90,100]
+for i in mynumbs:
+    print(i)
+
+
+
+
+mynumbs=[10,20,30,40,50,60,70,80,90,100]
+
+for i in mynumbs:
+    if i>50:
+        print('salam')
+
+
+
+#iteration--------------
+#man bekham brm too dele yechiz --> for
+#chjeck konam shartam -> if shart
+#dastoore?
+
+
+#--->1-->print kone
+
+
+mynumbs=[10,20,30,40,50,60,70,80,90,100]
+for i in mynumbs:
+    if i>50:
+        print(i)
+
+
+
+
+#2-->beshmor
+
+count=0
+mynumbs=[10,20,30,40,50,60,70,80,90,100]
+for i in mynumbs:
+    if i>50:
+        #print(i)
+        count=count+1
+
+
+print(count) #5
+
+
+
+#3--->bekeshesh brioon jdoash kon
+
+
+new_list=[]
+
+mynumbs=[10,20,30,40,50,60,70,80,90,100]
+for i in mynumbs:
+    if i>50:
+        new_list.append(i)
+
+
+
+
+print(new_list)
+
+#[60, 70, 80, 90, 100]
+
+
+
+#4-->hazf konam
+new_list=[]
+
+mynumbs=[10,20,30,40,50,60,70,80,90,100]
+for i in mynumbs:
+    if i<50:
+        new_list.append(i)
+
+
+#5-->dota karo
+
+up50=[]
+low50=[]
+
+
+mynumbs=[10,20,30,40,50,60,70,80,90,100]
+for i in mynumbs:
+    if i>50:
+        #berrizesh to ye list
+        up50.append(i)
+    else:
+        #too ye list dg
+        low50.append(i)
+        
+
+#---------------------------
+#-------
+
+mynumbs=[10,20,30,40,50,60,70,80,90,100]
+
+
+for i in mynumbs:
+    print(i)
+
+
+
+#ignore---> nadid begiram
+
+for i in mynumbs:
+    if i==30:
+        #skip
+        continue
+
+    print(i)
+
+
+'''
+10
+20
+40
+50
+60
+70
+80
+90
+100
+
+'''
+
+
+mynumbs=[10,20,30,40,50,60,70,80,90,100]
+
+
+for i in mynumbs:
+    print(i)
+    if i==30:
+        #skip
+        continue
+
+'''
+
+10
+20
+30
+40
+50
+60
+
+'''
+
+
+mynumbs=[10,20,30,40,50,60,70,80,90,100]
+
+for i in mynumbs:
+    if i==30:
+        break
+    print(i)
+    
+    
+
+#ghat mikone
+mynumbs=[10,20,30,40,50,60,70,80,90,100]
+for i in mynumbs:
+    print(i)
+    if i==30:
+        break
+
+'''
+10
+20
+30
+
+'''
+
+
+
+#---------
+'''
+-------RESERVED-----------
+---------KEYWORDS(BANAFSH)--------
+-----------IF......
+-----------LOOOPP------------
+
+---------------FOR-----------------
+TEKRAR
+ITERATION
+
+---------------WHILE-----------------
+TEKRAR
+ITERATION
+
+
+shomarande
+mahdoode
+dastooor
+
+    enteham-->baze ba yek shart mitoanam bazi koanm
+
+
+
+for shomarande in mahdoode:
+    dastooor
+    
+    
+for i in range(0,10):
+    print(i)
+    
+#mahdoode msohakahse
+#0 ta 9 boro 
+
+
+
+
+
+
+'''
+
+
+
+for i in range(0,10):
+    print(i)
+
+
+'''
+
+0
+1
+2
+3
+4
+5
+6
+7
+8
+
+'''
+
+
+#i --> 0 2 4 6 8 
+
+
+mylist=[0,2,4,6,8]
+
+for i in mylist:
+    print(i)
+
+
+#----
+
+#range()  []
+
+#start  end+1   step
+
+
+
+
+for i in range(0,10,1):
+    print(i)
+
+#mahdode-->0-->10  , 0 1 2 3 4 5 6 7 8 9
+
+
+for i in range(0,10):
+    print(i)
+
+
+for i in range(0,10,2):
+    print(i)
+
+#range(0,10,2) --> 0 2 4 6 8 
+
+#i->0  dastoor
+#0--2
+
+
+'''
+
+for shomarande in (start,end,step):
+    dastooor
+    
+    
+    
+
+shomarande -->tarif kon
+
+while shomarande<end:
+    dastooor
+    shoamrande=shjomarande+step
+    
+'''
+
+
+
+for i in range(0,10):
+    print('salam')
+    
+    
+for i in range(0,10,1):
+    print('salam')
+    
+    
+    
+
+i=0
+while i<10:
+    print('salam')
+    i=i+1
+    
+    
+
+    
+    
+
+for i in range(4,10,2):
+    print('salam')
+    
+    
+
+i=4
+while i<10:
+    print('salam')
+    i=i+2
+
+
+    
+
+
+
+
+
+'''
+
+
+shomarando --> besazi
+
+while shart --->sharte khateme--> zamani entehaye shomarande
+
+
+dastooreto
+
+shoamrande-->start
+
+shart ta zamani k shart True bashe halgeh ro anjam midam
+hamiin
+
+
+
+
+shomarande -->tarif 1**
+varede halgeh beshi--> 2**
+sharte khateme bezari 3***
+dastooret ejra she 4**
+
+
+'''
+
+
+
+while i<5:
+    print('salam')
+#NameError: name 'i' is not defined
+
+#shoro nadare
+#bi shorooo
+
+
+
+#asan varede halgeh nmishe
+
+i=10
+while i<5:
+    print('salam')
+
+
+
+
+#i=0
+#while i<5:
+#    print('salam')
+
+
+
+#mahdode --> az i=0 ta i=5 #.....
+
+#gofte start -->0
+
+#end --.shart 
+
+i=0
+while i<5:
+    print('salam')
+    i=i+1
+
+'''
+
+i-->0  salam , 1
+i-->1 salam , 2
+i-->2 salam ,3
+3--->salam ,4
+4-->salam , 5
+
+
+
+'''
+
+#------------
+#whilte true
+
+
+#shoamrande
+
+
+while False:
+    print('salam')
+
+#ejra
+
+
+##while True:
+#    print('salam')
+
+#if --> break 
+
+
+
+#while treu -->break
+
+
+#Mohemtarin applciation
+#for---> iteration
+#while-->while true--> ta felan zaman felan kon
+
+#ta vaghty k karbar eshtebah dade felano kon
+
+
+
+
+
+i=0
+while i<5:
+    print('salam')
+    i=i+1
+    
+    
+    
+
+
+
+i=0
+while True:
+    if i>=5:
+        break
+    print('salam')
+    i=i+1
+
+
+
+
+'''
+salam
+salam
+salam
+salam
+salam
+salam
+
+'''
+
+
+#==========================
+code=input('lotfan code mahsol bede:')
+name=input('lotfan name mahsol bede:')
+price=input('lotfan gheymate mahsool bede')
+
+#tra zamani k poric edoro nabashe felan kon
+
+
+
+while not price.isdigit():
+    price=input('lotfan dobare adadet ro bzn')
+    
+
+
+price=float(price)
+text=f'''
+----plutus management----
+///////Information///////
+
+product code: {code}
+product name: {name}
+    
+    Total price: {price*0.7}
+    
+
+'''
+print(text)
+
+answer=input('do you confirm your product information? (yes or no):')
+
+
+
+#---------
+#==========================
+code=input('lotfan code mahsol bede:')
+name=input('lotfan name mahsol bede:')
+
+#tra zamani k poric edoro nabashe felan kon
+
+
+while True:
+    price=input('lotfan gheymate mahsool bede')
+    
+    if price.isdigit():
+        break
+    
+    
+    
+
+
+price=float(price)
+text=f'''
+----plutus management----
+///////Information///////
+
+product code: {code}
+product name: {name}
+    
+    Total price: {price*0.7}
+    
+
+'''
+print(text)
+
+answer=input('do you confirm your product information? (yes or no):')
+
+
+
+
+'''
+Task1---------------
+
+hamon aval k taraf price ro mizane
+check kone digit dare ya na
+age na --> bejaye inke bege mojadad emtehan kon
+ta zamani k taraf doros gheymat vared kone ,
+hey azash price bekhad
+
+
+
+TASK2--------------
+bad azinkie takm,il kardi
+
+bia too no -->
+agte taraf zad no--> dobare soalaro az aval beporse
+ta zamani k taraf bezane yes
+
+
+
+
+TASK3 (ADVANCED)-------
+har dotaye ghablio anjam bede
+ama tedad talashe namovafagh ro hesab kone
+ag ishtar aza 3 bar
+yek code mahsole moshabe ro 
+hey gof na -->bege mojadad emtehan kon
+
+
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
